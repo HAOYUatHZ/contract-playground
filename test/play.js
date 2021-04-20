d02d183a3eee699f247aad967919291c5f4af0b8
HAOYUatHZ <haoyu@protonmail.com>
2021-04-20 16:01:10 +0800

    init other tests

--- test/play.js
+++ test/play.js
@@ -12,8 +12,27 @@ describe("play", () => {
     // await playToken.initialize();
   });
 
-  it("playing", async function () {
+  it("playing connect", async function () {
     await expect(await acc1.getBalance()).to.equal("9999985767784000000000");
-    // await playToken.connect(acc1).approve(fluidex.address, 1000);
+    await playToken
+      .connect(acc1)
+      .depositETH(acc1addr, { value: "999985767784000000000" });
+    await expect(await acc1.getBalance()).to.equal("8999999799800000000000");
+    await playToken
+      .connect(acc1)
+      .withdrawETH(acc1addr, "999985767784000000000");
+    await expect(await acc1.getBalance()).to.equal("9999985272232000000000");
+  });
+
+  it("playing call", async function () {
+    await expect(await acc1.getBalance()).to.equal("9999985272232000000000");
+    await playToken
+      .connect(acc1)
+      .depositETH(acc1addr, { value: "999985767784000000000" });
+    await expect(await acc1.getBalance()).to.equal("8999999304248000000000");
+    await playToken
+      .connect(acc1)
+      .withdrawETH(acc1addr, "999985767784000000000");
+    await expect(await acc1.getBalance()).to.equal("9999984776680000000000");
   });
 });
