1dd030c5c38a86f07d970929cfbfd3c158f64316
HAOYUatHZ <haoyu@protonmail.com>
2021-04-20 15:46:05 +0800

    check balance

--- test/play.js
+++ test/play.js
@@ -2,6 +2,9 @@ const { expect } = require("chai");
 
 describe("play", () => {
   before(async () => {
+    [acc1] = await ethers.getSigners();
+    acc1addr = acc1.address;
+
     const playTokenFactory = await ethers.getContractFactory("PlayToken");
     playToken = await playTokenFactory.deploy(1000);
 
@@ -9,5 +12,8 @@ describe("play", () => {
     // await playToken.initialize();
   });
 
-  it("playing", async function () {});
+  it("playing", async function () {
+    await expect(await acc1.getBalance()).to.equal("9999985767784000000000");
+    // await playToken.connect(acc1).approve(fluidex.address, 1000);
+  });
 });
