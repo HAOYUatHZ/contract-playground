7522456091fa77da044363a31c71df2aebd3c673
HAOYUatHZ <haoyu@protonmail.com>
2021-04-20 15:14:34 +0800

    deploy contract

--- test/play.js
+++ test/play.js
@@ -3,6 +3,12 @@ const { expect } = require("chai");
 describe("play", () => {
 
   before(async () => {
+
+    const playTokenFactory = await ethers.getContractFactory("PlayToken");
+    playToken = await playTokenFactory.deploy(1000);
+
+    await playToken.deployed();
+    // await playToken.initialize();
   });
 
   it("playing", async function () {
