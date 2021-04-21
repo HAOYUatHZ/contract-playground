d26b0b49c9144fe4de44c8561c7982076a9751ed
HAOYUatHZ <haoyu@protonmail.com>
2021-04-21 14:52:09 +0800

    add more config and scripts

---
+++ scripts/topup.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  let myAddr = "0xFFd6d35996A1049ECAe88B62a92E0e2a36685990";
+  let value = ethers.utils.parseUnits('10', 'ether').toHexString();
+
+  [account] = await ethers.getSigners();
+  await account.sendTransaction({to: myAddr, value: value});
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
