b7345b37c3f92269176b151c36a546bfb5eacddf
HAOYUatHZ <haoyu@protonmail.com>
2021-05-06 10:32:27 +0800

    add more deployment scripts

---
+++ scripts/deploy_token.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  [account] = await ethers.getSigners();
+  accountAddr = account.address;
+  const erc20Factory = await ethers.getContractFactory("PlayToken");
+  const initialSupply = 1000;
+  erc20Mock = await erc20Factory.deploy(
+    initialSupply
+  );
+  await erc20Mock.deployed();
+  console.log("PlayToken deployed to:", erc20Mock.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
