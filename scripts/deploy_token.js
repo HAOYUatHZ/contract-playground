a96284a771cbe293374b169641f3f20e37d8aafb
HAOYUatHZ <haoyu@protonmail.com>
2021-05-06 10:33:18 +0800

    add more docs

--- scripts/deploy_token.js
+++ scripts/deploy_token.js
@@ -6,6 +6,14 @@
 const hre = require("hardhat");
 
 async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
   [account] = await ethers.getSigners();
   accountAddr = account.address;
   const erc20Factory = await ethers.getContractFactory("PlayToken");
